b472c35aa9aaa5f9431d45e25c983ac2dfe315e9
majiabao <majiabaoIT@163.com>
2018-01-23 11:34:52 +0800

    C

---
+++ propertyplace/src/main/java/com/jiabao/WebConfig.java
@@ -0,0 +1,12 @@
+package com.jiabao;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
+
+/**
+ * @author majiabao on 2018/1/23.
+ */
+@Configuration
+public class WebConfig extends DelegatingWebMvcConfiguration {
+
+}
